213502a09bc35f66fc9fa4bb4e244e30c70b52e8
NT-marlowe <n.tezuka196.ninj@gmail.com>
2023-02-19 18:19:52 +0900

    Intel bluetooth adoptor is irreadable

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use rusb;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    for device in rusb::devices().unwrap().iter() {
+        let device_descriptor = device.device_descriptor().unwrap();
+
+        println!(
+            "Bus {:03} Device {:03} ID {:04x}:{:04x}",
+            device.bus_number(),
+            device.address(),
+            device_descriptor.vendor_id(),
+            device_descriptor.product_id()
+        );
+
+        let handle = match device.open() {
+            Ok(handle) => handle,
+            Err(e) => panic!("{}", e),
+        };
+
+        let timeout = Duration::from_secs(1);
+        let language = handle.read_languages(timeout).unwrap()[0];
+
+        // println!("lang:{:?}", language);
+        // println!("descri:{:?}", device_descriptor.product_string_index());
+        let product_str = match handle.read_product_string(language, &device_descriptor, timeout) {
+            Ok(s) => s,
+            Err(_) => String::from("Error: unable to read string"),
+        };
+
+        println!("Product: {}", product_str);
+        println!("-----------------------------")
+    }
 }

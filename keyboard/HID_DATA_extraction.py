eae86364976c6a1caf2a35b1d4fbc236ccfa6a53
Ichinose-Kazuki <ichinose-kazuki657@g.ecc.u-tokyo.ac.jp>
2023-03-07 01:51:29 +0900

    Prints pushed key. Supports only DZ60.

--- keyboard/HID_DATA_extraction.py
+++ keyboard/HID_DATA_extraction.py
@@ -3,13 +3,18 @@ import json
 keyboard = input('Keyboard name: ')
 dirname = keyboard
 file_base_name = keyboard
-json_file = open('./{}/{}.json'.format(dirname, file_base_name), 'r')
-json_data = json.load(json_file)
-json_file.close()
+with open('./{}/{}.json'.format(dirname, file_base_name), 'r') as json_file:
+    json_data = json.load(json_file)
+
+with open('./{}/{}-keys.txt'.format(dirname, file_base_name), 'r') as keys_file:
+    keys = [line.rstrip() for line in keys_file]
 
 save_file_name = './{}/{}-hid-data.txt'.format(dirname, file_base_name)
 save_file = open(save_file_name, 'w')
-for d in json_data:
-    hiddata = d['_source']['layers']['usbhid.data'].replace(':', '')
-    save_file.write(hiddata+'\n')
+for i in range(len(json_data)):
+    jd = json_data[i]
+    hiddata = jd['_source']['layers']['usbhid.data'].split(':')
+    for hd in hiddata:
+        save_file.write(format(int(hd), '08b'))
+    save_file.write(',{}\n'.format(keys[i]))
 save_file.close()
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 mod frame;
 
+use std::fs::File;
+use std::time::Duration;
+use std::path::Path;
+use std::io::{self, BufRead};
+
 use rusb;
 
 fn convert_argument(input: &str) -> (u8, u8, u8) {
@@ -15,6 +20,40 @@ fn convert_argument(input: &str) -> (u8, u8, u8) {
     return (ret[0], ret[1], ret[2]);
 }
 
+fn decode_protocol(buf: & [u8], product_name: String) -> String {
+    let mut buf_binary = String::new();
+    for byte in buf {
+        buf_binary.push_str(
+            &format!("{byte:08b}")
+        );
+    }
+    let str_hid_data = 
+        format!("./keyboard/{pn}/{pn}-hid-data.txt", pn=product_name)
+    ;
+
+    let mut pushed_key = String::from("");
+
+    if let Ok(lines) = read_lines(str_hid_data) {
+        for line in lines {
+            if let Ok(data) = line {
+                let mut split_data = data.split(',').into_iter();
+                let (hid_data, key) = (split_data.next().unwrap(), split_data.next().unwrap());
+                if hid_data == buf_binary {
+                    pushed_key = key.to_string();
+                    break;
+                }
+            }
+        }
+    }
+    pushed_key
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
 
@@ -25,11 +64,10 @@ fn main() {
 
     let (target_bus_num, target_address, target_endpoint) = convert_argument(args[1].as_ref());
 
-    let mut buf: [u8; 256] = [0; 256];
     for mut device in rusb::devices().unwrap().iter() {
         let device_descriptor = device.device_descriptor().unwrap();
 
-        println!(
+        print!(
             "Bus {:03} Device {:03} ID {:04x}:{:04x}",
             device.bus_number(),
             device.address(),
@@ -37,35 +75,44 @@ fn main() {
             device_descriptor.product_id()
         );
 
+        let mut handle = match device.open() {
+            Ok(handle) => handle,
+            Err(e) => panic!("{}", e),
+        };
+
+        let timeout = Duration::from_millis(100);
+        let product_name = 
+            handle.read_product_string(
+                handle.read_languages(timeout).unwrap()[0],
+                &device_descriptor,
+                timeout
+            ).unwrap();
+
+        println!(" Name {}", product_name);
+
         if device.bus_number() != target_bus_num
             || device.address() != target_address
         {
             continue;
         }
 
-        let mut handle = match device.open() {
-            Ok(handle) => handle,
-            Err(e) => panic!("{}", e),
+        let bufsize = match product_name.as_str() {
+            "DZ60" => 32,
+            _ => 8,
         };
+        let mut buf: [u8; 256] = [0; 256];
 
         match frame::read_ascii_array(
             &mut device,
             device_descriptor,
             &mut handle,
             rusb::TransferType::Interrupt,
-            &mut buf,
+            &mut buf[0..bufsize],
             target_endpoint,
         ) {
-            Ok(n) => {
-                println!("n is {}", n);
-                println!("Received data: {:?}", &buf[0..32]);
-                println!("Received data: {:?}", &buf[32..64]);
-                println!("Received data: {:?}", &buf[64..96]);
-                println!("Received data: {:?}", &buf[96..128]);
-                println!("Received data: {:?}", &buf[128..160]);
-                println!("Received data: {:?}", &buf[160..192]);
-                println!("Received data: {:?}", &buf[192..224]);
-                println!("Received data: {:?}", &buf[224..256]);
+            Ok(_) => {
+                // println!("{:?}", buf);
+                println!("{}", decode_protocol(&buf[0..bufsize], product_name));
             },
             Err(e) => panic!("{}", e),
         }
